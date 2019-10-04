f4a6c703d653d5fe5098b83019cc5775930a9332
Nicorizky <xsirhaan@gmail.com>
2019-10-04 08:20:56 +0700

    Initial commit

---
+++ application/views/templates/header_menu.php
@@ -0,0 +1,16 @@
+<header class="main-header">
+    <!-- Logo -->
+    <a href="index2.html" class="logo">
+      <span class="logo-lg"><b>Peradminan</b></span>
+    </a>
+    <!-- Header Navbar: style can be found in header.less -->
+    <nav class="navbar navbar-static-top">
+      <!-- Sidebar toggle button-->
+      <a href="#" class="sidebar-toggle" data-toggle="push-menu" role="button">
+        <span class="sr-only">Toggle navigation</span>
+      </a>
+
+    </nav>
+  </header>
+  <!-- Left side column. contains the logo and sidebar -->
+  
